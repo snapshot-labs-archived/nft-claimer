7c292e71722bac9719ec98ca1b576b23c1106f72
Scott Piriou <30843220+pscott@users.noreply.github.com>
2023-07-03 13:07:21 +0200

    chore: add mint to deploy script

--- script/DeployScript.s.sol
+++ script/DeployScript.s.sol
@@ -6,12 +6,20 @@ import { SpaceCollectionFactory } from "../src/SpaceCollectionFactory.sol";
 import { SpaceCollection } from "../src/SpaceCollection.sol";
 import { Digests } from "../test/utils/Digests.sol";
 import { NO_UPDATE_U8, NO_UPDATE_ADDRESS } from "../test/utils/BaseCollection.sol";
+import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 // solhint-disable-next-line max-states-count
 contract DeployScript is Script {
+    string constant FACTORY_NAME = "SpaceCollectionFactory";
+    string constant FACTORY_VERSION = "0.1";
+    string constant COLLECTION_NAME = "TestDAO";
+    string constant COLLECTION_VERSION = "0.1";
+
+    // Goerli WETH
+    IERC20 private constant WETH = IERC20(0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6);
+
     function run() public {
-        string memory FACTORY_NAME = "SpaceCollectionFactory";
-        string memory FACTORY_VERSION = "0.1";
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         address deployerAddr = vm.addr(deployerPrivateKey);
         uint8 proposerFee = 10;
@@ -35,8 +43,8 @@ contract DeployScript is Script {
         bytes4 SPACE_INITIALIZE_SELECTOR = 0x977b0efb;
         bytes memory initializer = abi.encodeWithSelector(
             SPACE_INITIALIZE_SELECTOR,
-            "TestDAO",
-            "0.1",
+            COLLECTION_NAME,
+            COLLECTION_VERSION,
             maxSupply,
             mintPrice,
             proposerFee,
@@ -64,9 +72,96 @@ contract DeployScript is Script {
 
         factory.deployProxy(implem, initializer, salt, v, r, s);
 
+        SpaceCollection collection = SpaceCollection(_predictProxyAddress(address(factory), implem, salt));
+        WETH.approve(address(collection), mintPrice * 3); // Should mint three times
+
+        uint256 proposalId = 1;
+        _mint_one(collection, deployerAddr, deployerAddr, proposalId, salt);
+
+        salt += 1;
+        address[] memory proposers = new address[](2);
+        proposers[0] = deployerAddr;
+        proposers[1] = deployerAddr;
+
+        uint256[] memory proposalIds = new uint256[](2);
+        proposalIds[0] = 2;
+        proposalIds[1] = 3;
+
+        _mint_batch(collection, proposers, deployerAddr, proposalIds, salt);
+
         factory.updateFactorySettings(NO_UPDATE_U8, NO_UPDATE_ADDRESS, NO_UPDATE_ADDRESS, verifiedSigner);
         factory.transferOwnership(verifiedSigner);
 
         vm.stopBroadcast();
+    }
+
+    function _mint_one(
+        SpaceCollection collection,
+        address proposer,
+        address recipient,
+        uint256 proposalId,
+        uint256 salt
+    ) internal {
+        bytes32 digest = Digests._getMintDigest(
+            COLLECTION_NAME,
+            COLLECTION_VERSION,
+            address(collection),
+            proposer,
+            recipient,
+            proposalId,
+            salt
+        );
+
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(deployerPrivateKey, digest);
+
+        collection.mint(proposer, proposalId, salt, v, r, s);
+    }
+
+    function _mint_batch(
+        SpaceCollection collection,
+        address[] memory proposers,
+        address recipient,
+        uint256[] memory proposalIds,
+        uint256 salt
+    ) internal {
+        bytes32 digest = Digests._getMintBatchDigest(
+            COLLECTION_NAME,
+            COLLECTION_VERSION,
+            address(collection),
+            proposers,
+            recipient,
+            proposalIds,
+            salt
+        );
+
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(deployerPrivateKey, digest);
+
+        collection.mintBatch(proposers, proposalIds, salt, v, r, s);
+    }
+
+    function _predictProxyAddress(
+        address _factory,
+        address _implementation,
+        uint256 _salt
+    ) internal pure returns (address) {
+        return
+            address(
+                uint160(
+                    uint(
+                        keccak256(
+                            abi.encodePacked(
+                                bytes1(0xff),
+                                _factory,
+                                bytes32(_salt),
+                                keccak256(
+                                    abi.encodePacked(type(ERC1967Proxy).creationCode, abi.encode(_implementation, ""))
+                                )
+                            )
+                        )
+                    )
+                )
+            );
     }
 }
